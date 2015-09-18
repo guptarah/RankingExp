921353b922dc4f436bf911ead640d58030cbb9f9
guptarah <guptarah@usc.edu>
2015-09-18 16:54:10 -0700

    editing

--- EnsembleRanker/SVRanker/SVRankerSoft.py
+++ EnsembleRanker/SVRanker/SVRankerSoft.py
@@ -6,6 +6,14 @@ where k is the probability that x_g is higher ranked than x_l
 import numpy
 import sys
 
+def get_gradient(features,w,soft_scores):
+   D = features.shape[1]
+   N = features.shape[0]
+   hyperplane_dist = numpy.dot(features,w.T)
+   hinge_dist = numpy.array(numpy.ones((hyperplane_dist.shape)) - hyperplane_dist) 
+   wtd_features = numpy.multiply(features,numpy.tile(soft_scores,(1,D)))
+   grad = -1*numpy.sum(wtd_features[hinge_dist[:,0]>0,:],axis=0)/N
+   return grad
 
 def svr_optimization(features,soft_scores,w_init=0,learning_rate=0.1,n_epochs=100,lambda_w=.01):
 
@@ -17,27 +25,34 @@ def svr_optimization(features,soft_scores,w_init=0,learning_rate=0.1,n_epochs=10
    if soft_scores.shape[0] != N:
       print 'count of soft scores is not equal to number of datapoints'
 
-   ext_features = numpy.hstack((features,numpy.ones((features.shape[0],1))))
- 
+   ext_features_g = numpy.hstack((features,numpy.ones((features.shape[0],1))))
+   ext_features_l = numpy.hstack((-1*features,numpy.ones((features.shape[0],1)))) 
+
    w = w_init 
    for i in range(n_epochs): 
-      # computing cost for k*[1-z{w.T*(x_g-x_l)}] part
-      hyperplane_dist_g = numpy.dot(ext_features,w.T)
-      wtd_hinge_dist_g = numpy.array(numpy.multiply(soft_scores, (numpy.ones((hyperplane_dist_g.shape)) - hyperplane_dist_g)))
-      cost_g = numpy.sum(wtd_hinge_dist_g[wtd_hinge_dist_g[:,0]>0])/N + lambda_w*numpy.dot(w,w.T)
-      grad_g = -1*numpy.sum(ext_features[wtd_hinge_dist_g[:,0]>0,:],axis=0)/N 
-
-      # computing cost for (1-k)[1-z{w.T*(x_l-x_g)}] part
-      hyperplane_dist_l = -1*numpy.dot(ext_features,w.T)
-      soft_scores_l = numpy.ones((soft_scores.shape))-soft_scores
-      wtd_hinge_dist_l = numpy.array(numpy.multiply(soft_scores_l, (numpy.ones((hyperplane_dist_l.shape)) - hyperplane_dist_l)))
-      cost_l = numpy.sum(wtd_hinge_dist_l[wtd_hinge_dist_l[:,0]>0])/N + lambda_w*numpy.dot(w,w.T)
-      grad_l = -1*numpy.sum(ext_features[wtd_hinge_dist_l[:,0]>0,:],axis=0)/N 
-
+#      # computing cost for k*[1-z{w.T*(x_g-x_l)}] part
+#      hyperplane_dist_g = numpy.dot(ext_features_g,w.T)
+#      wtd_hinge_dist_g = numpy.array(numpy.multiply(soft_scores, (numpy.ones((hyperplane_dist_g.shape)) - hyperplane_dist_g)))
+#      cost_g = numpy.sum(wtd_hinge_dist_g[wtd_hinge_dist_g[:,0]>0])/N + lambda_w*numpy.dot(w,w.T)
+#      wtd_ext_features_g = numpy.multiply(ext_features_g,numpy.tile(soft_scores,(1,D+1)))
+#      grad_g = -1*numpy.sum(wtd_ext_features_g[wtd_hinge_dist_g[:,0]>0,:],axis=0)/N 
+#
+#      # computing cost for (1-k)[1-z{w.T*(x_l-x_g)}] part
+#      hyperplane_dist_l = numpy.dot(ext_features_l,w.T)
+#      soft_scores_l = numpy.ones((soft_scores.shape))-soft_scores
+#      wtd_hinge_dist_l = numpy.array(numpy.multiply(soft_scores_l, (numpy.ones((hyperplane_dist_l.shape)) - hyperplane_dist_l)))
+#      cost_l = numpy.sum(wtd_hinge_dist_l[wtd_hinge_dist_l[:,0]>0])/N + lambda_w*numpy.dot(w,w.T)
+#      wtd_ext_features_l = numpy.multiply(ext_features_l,numpy.tile(soft_scores_l,(1,D+1)))
+#      grad_l = -1*numpy.sum(wtd_ext_features_l[wtd_hinge_dist_l[:,0]>0,:],axis=0)/N 
+#
+#      grad = grad_l + grad_g + 2*lambda_w*w
+      
+      grad_g = get_gradient(ext_features_g,w,soft_scores)
+      soft_scores_l = numpy.ones(soft_scores.shape) - soft_scores
+      grad_l = get_gradient(ext_features_l,w,soft_scores_l)
       grad = grad_l + grad_g + 2*lambda_w*w
-
       w = w - learning_rate*grad
- 
+
    return w 
 
 if __name__ == "__main__":
